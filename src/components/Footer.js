f3d7c3035c36585fd73f2001b0129f102d40d9c8
venkatsaketh <vvsaketh24@gmail.com>
2023-09-13 22:39:50 +0530

    responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
   const activeTodos = todos.filter((x) => !x.completed);
   const filter = useSelector((store) => store.filter);
   return (
-    <div className="border-t-2 border-cyan-500 flex justify-between px-3 py-4 mx-2 sm:mx-0 sm:px-6">
-      <p className="w-5/12 md:w-6/12">
+    <div className="border-t-2 border-cyan-500 sm:flex justify-between px-3 py-4 mx-4 sm:mx-0 sm:px-6">
+      <p className="sm:w-4/12 md:w-6/12 text-center sm:text-start">
         {activeTodos.length} remaining todo{activeTodos.length > 1 ? "s" : ""}
       </p>
-      <div className="w-7/12 md:w-6/12 ml-2 sm:ml-0 flex justify-between">
+      <div className="mt-5 sm:mt-0 sm:w-7/12 md:w-6/12  flex justify-between">
         <button
           className={
             "border-gray-500 border-2 px-1 sm:px-3 rounded-md hover:border-cyan-700 " +
